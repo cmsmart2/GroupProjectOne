71b164bbe4aff3aded5f8536efbf389fa9c12c7d
Tiur Mawaddah <tiurzm@gmail.com>
2019-10-17 17:01:24 -0500

    <comment>

--- assets/java/function.js
+++ assets/java/function.js
@@ -4,16 +4,18 @@ $("#add-ingreditien").on("click", function(event){
     event.preventDefault();
     var ingreditien = $("#ingreditien").val();
     var amount = $("#amount").val();
-    var list = $("<ul>").append($("<li>").text(ingreditien+" "+amount));
+    var list = $("<li>");
+    list.text(ingreditien+" "+amount);
     $("#ingreditients-here").append(list);
     $("#ingreditien").val(" ");
     $("#amount").val(" ");
-});
 
-// Take the ingreditiens to find some recipes
+    // Take the ingreditiens to find some recipes
 $("#find-recipe").on("click", function(event){
     event.preventDefault();
     alert("CLICKED");
     var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=e53c0977ab3a4a5b8872e1c7efb889ce&ingredients=apples,+flour,+sugar&number=2"
 
-})
+});
+});
+
