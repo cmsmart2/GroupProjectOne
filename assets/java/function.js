2419fbf3560aa3f5ec920b5824ea63fa6a409577
Tiur Mawaddah <tiurzm@gmail.com>
2019-10-17 18:56:23 -0500

    <comment>

--- assets/java/function.js
+++ assets/java/function.js
@@ -5,17 +5,28 @@ $("#add-ingreditien").on("click", function(event){
     var ingreditien = $("#ingreditien").val();
     var amount = $("#amount").val();
     var list = $("<li>");
+    list.addClass("find");
+    list.attr("data-ingreditien", ingreditien);
+    list.attr("data-amount", amount);
     list.text(ingreditien+" "+amount);
     $("#ingreditients-here").append(list);
     $("#ingreditien").val(" ");
     $("#amount").val(" ");
 
     // Take the ingreditiens to find some recipes
-$("#find-recipe").on("click", function(event){
-    event.preventDefault();
-    alert("CLICKED");
-    var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=e53c0977ab3a4a5b8872e1c7efb889ce&ingredients=apples,+flour,+sugar&number=2"
+    $("#find-recipe").on("click", function(event){
+        event.preventDefault();
+        var findIngredients = $(".find").attr("data-ingreditien");
+        console.log(findIngredients);
+        var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=e53c0977ab3a4a5b8872e1c7efb889ce&ingredients="+ findIngredients + "&number=10"
+        
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }) .then(function(response){
+            console.log(response)
+        });
+    });
 
-});
 });
 
