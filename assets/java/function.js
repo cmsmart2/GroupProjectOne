9cb6514ecce89a6e990ef80d291ac03d6a916e54
Tiur Mawaddah <tiurzm@gmail.com>
2019-10-17 16:47:05 -0500

    <COMMENT>

---
+++ assets/java/function.js
@@ -0,0 +1,19 @@
+// Take value from inputs
+
+$("#add-ingreditien").on("click", function(event){
+    event.preventDefault();
+    var ingreditien = $("#ingreditien").val();
+    var amount = $("#amount").val();
+    var list = $("<ul>").append($("<li>").text(ingreditien+" "+amount));
+    $("#ingreditients-here").append(list);
+    $("#ingreditien").val(" ");
+    $("#amount").val(" ");
+});
+
+// Take the ingreditiens to find some recipes
+$("#find-recipe").on("click", function(event){
+    event.preventDefault();
+    alert("CLICKED");
+    var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=e53c0977ab3a4a5b8872e1c7efb889ce&ingredients=apples,+flour,+sugar&number=2"
+
+})
