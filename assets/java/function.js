a8ceb4adde851819d0498fe0e8607fb90f73424f
Zachary <Omitakura@gmail.com>
2019-10-19 12:39:55 -0500

    updated edamam call to display beginning nutritional value in ingredient box

--- assets/java/function.js
+++ assets/java/function.js
@@ -1,5 +1,5 @@
 // Take value from inputs and look for recipe based on that value
-$("#add-ingreditien").on("click", function(event){
+$("#add-ingreditien").on("click", function (event) {
     event.preventDefault();
     var ingreditien = $("#ingreditien").val();
     var amount = $("#amount").val();
@@ -7,13 +7,13 @@ $("#add-ingreditien").on("click", function(event){
     list.addClass("find");
     list.attr("data-ingreditien", ingreditien);
     list.attr("data-amount", amount);
-    list.text(ingreditien+" "+amount);
+    list.text(ingreditien + " " + amount);
     $("#ingreditients-here").append(list);
     $("#ingreditien").val(" ");
     $("#amount").val(" ");
 
     // Take the ingreditiens to find some recipes
-    $("#find-recipe").on("click", function(event){
+    $("#find-recipe").on("click", function (event) {
         event.preventDefault();
         var findIngredients = $(".find").attr("data-ingreditien");
         console.log(findIngredients);
@@ -22,33 +22,33 @@ $("#add-ingreditien").on("click", function(event){
         // Tiur's API Key
         // var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=e53c0977ab3a4a5b8872e1c7efb889ce&ingredients="+ findIngredients + "&number=10"
         // Cera's API Key
-        var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=bf050a8943b74210a77973e2062818b1&ingredients="+ findIngredients + "&number=12"
+        var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=bf050a8943b74210a77973e2062818b1&ingredients=" + findIngredients + "&number=12"
 
         $.ajax({
             url: queryURL,
             method: "GET"
-        }) .then(function(response){
+        }).then(function (response) {
             console.log(response);
             var results = response.length;
             console.log(results);
-        for (var i = 0; i < results; i++) {
-            var recipePic = response[i].image;
-            var recipeId = response[i].id;
-            var recipeDiv = $("<div>");
-            recipeDiv.addClass("card recipe-card");
-            recipeDiv.addClass("col-lg-3");
-            recipeDiv.css("max-width", "350px");
-            var recipeImage = $("<img>");
-            recipeImage.attr("src", recipePic);
-            recipeImage.attr("data-id", recipeId);
-            recipeImage.addClass("card-img-top");
-            recipeImage.addClass("mx-auto");
-            recipeImage.css("max-width", "300px");
-            var p = $("<p>").text(response[i].title);
-            p.addClass("card-text");
-            p.addClass("text-center");
-            recipeDiv.append(recipeImage, p);
-            $("#recipe-here").append(recipeDiv);
+            for (var i = 0; i < results; i++) {
+                var recipePic = response[i].image;
+                var recipeId = response[i].id;
+                var recipeDiv = $("<div>");
+                recipeDiv.addClass("card recipe-card");
+                recipeDiv.addClass("col-lg-3");
+                recipeDiv.css("max-width", "350px");
+                var recipeImage = $("<img>");
+                recipeImage.attr("src", recipePic);
+                recipeImage.attr("data-id", recipeId);
+                recipeImage.addClass("card-img-top");
+                recipeImage.addClass("mx-auto");
+                recipeImage.css("max-width", "300px");
+                var p = $("<p>").text(response[i].title);
+                p.addClass("card-text");
+                p.addClass("text-center");
+                recipeDiv.append(recipeImage, p);
+                $("#recipe-here").append(recipeDiv);
             }
         });
     });
@@ -56,24 +56,41 @@ $("#add-ingreditien").on("click", function(event){
 });
 
 // get the recipe based on which recipe we choose
-$(document).on("click", ".card-img-top", function(event){
+$(document).on("click", ".card-img-top", function (event) {
     event.preventDefault();
-        var recipeIdNum = $(this).attr("data-id");
-        console.log(recipeIdNum);
-        // Tiur's API Key
-        // var queryURL = "https://api.spoonacular.com/recipes/" +recipeIdNum + "/information?apiKey=e53c0977ab3a4a5b8872e1c7efb889ce"
-        // Cera's API Key
-        var queryURL = "https://api.spoonacular.com/recipes/" +recipeIdNum + "/information?apiKey=bf050a8943b74210a77973e2062818b1"
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }) .then(function(response){
-            console.log(response);
-            console.log(response.analyzedInstructions[0].steps[0].step);
-            console.log(response.analyzedInstructions[0].steps[0].equipment[0].name);
-            console.log(response.analyzedInstructions[0].steps[0].ingredients[0].name);
-        });
+    var recipeIdNum = $(this).attr("data-id");
+    console.log(recipeIdNum);
+    // Tiur's API Key
+    // var queryURL = "https://api.spoonacular.com/recipes/" +recipeIdNum + "/information?apiKey=e53c0977ab3a4a5b8872e1c7efb889ce"
+    // Cera's API Key
+    var queryURL = "https://api.spoonacular.com/recipes/" + recipeIdNum + "/information?apiKey=bf050a8943b74210a77973e2062818b1"
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        console.log(response.analyzedInstructions[0].steps[0].step);
+        console.log(response.analyzedInstructions[0].steps[0].equipment[0].name);
+        console.log(response.analyzedInstructions[0].steps[0].ingredients[0].name);
+    });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -83,14 +100,25 @@ $("#find-nutrition").on("click", function (event) {
     event.preventDefault();
     var findIngredient = $(".find").attr("data-ingreditien");
     var findAmount = $(".find").attr("data-amount");
-    var findNutrition = findIngredient + findAmount;
+    var findNutrition = "%20" + findIngredient + "%20" + findAmount;
     var queryURL = "https://api.edamam.com/api/nutrition-data?app_id=303b58c4&app_key=ef9d7b4d891b056959de013622064837&ingr=" + findNutrition;
+    console.log(queryURL);
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response)
-        console.log(response.calories);
+        // console.log(response.calories);
+        var nutrition = response;
+        var calories = nutrition.calories;
+        var calcium = nutrition.totalDaily.CA
+        $('p').text(calories).appendTo("#ingreditients-here");
+
+        // for (var n = 0; n < nutrition; n++) {
+        // calories = response[n].calories;
+
+        // }
+        console.log(calories, calcium);
     });
 }); 
