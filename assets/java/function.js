75255dc527426db69ac30b68c329591952f54b88
Cera Smart <cera_smart@yahoo.com>
2019-10-23 22:35:08 -0500

    switched to Tiur key

--- assets/java/function.js
+++ assets/java/function.js
@@ -43,9 +43,9 @@ $("#add-ingredient").on("click", function (event) {
 $("#find-recipe").on("click", function (event) {
     event.preventDefault();
     // Tiur's API Key
-    // var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=e53c0977ab3a4a5b8872e1c7efb889ce&ingredients="+ arrayIng + "&number=5"
+    var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=e53c0977ab3a4a5b8872e1c7efb889ce&ingredients="+ arrayIng + "&number=5"
     // Cera's API Key
-    var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=bf050a8943b74210a77973e2062818b1&ingredients=" + arrayIng + "&number=5"
+    // var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=bf050a8943b74210a77973e2062818b1&ingredients=" + arrayIng + "&number=5"
     // Zach's API Key
     // var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=75d00f17ac79400eaeb4e9097fdcbdc6&ingredients="+ arrayIng + "&number=5"
     $.ajax({
@@ -140,9 +140,9 @@ $(document).on("click", ".d-block", function (event) {
     event.preventDefault();
     var recipeIdNum = $(this).attr("data-id");
     // Tiur's API Key
-    // var queryURL = "https://api.spoonacular.com/recipes/" +recipeIdNum + "/information?apiKey=e53c0977ab3a4a5b8872e1c7efb889ce"
+    var queryURL = "https://api.spoonacular.com/recipes/" +recipeIdNum + "/information?apiKey=e53c0977ab3a4a5b8872e1c7efb889ce"
     // Cera's API Key
-    var queryURL = "https://api.spoonacular.com/recipes/" + recipeIdNum + "/information?apiKey=bf050a8943b74210a77973e2062818b1"
+    // var queryURL = "https://api.spoonacular.com/recipes/" + recipeIdNum + "/information?apiKey=bf050a8943b74210a77973e2062818b1"
     // Zach's API Key
     // var queryURL = "https://api.spoonacular.com/recipes/" +recipeIdNum + "/information?apiKey=75d00f17ac79400eaeb4e9097fdcbdc6"
     $.ajax({
